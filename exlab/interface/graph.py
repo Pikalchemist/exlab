9ef8686f25a0b90a7246a22fe17c7ae35f30c72c
Alexandre Manoury <alexandre.manoury@gmail.com>
2020-09-19 17:02:42 +0200

    Add new graph options

--- exlab/interface/graph.py
+++ exlab/interface/graph.py
@@ -1,10 +1,16 @@
 # from exlab.remote.server import Server
+import math
+
+from exlab.utils.io import parameter
+
+import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
-def display(graph):
-    return Visual().display(graph)
+def display(*graphes):
+    return Visual().display(*graphes)
 
 
 class Visual(object):
@@ -12,28 +18,81 @@ class Visual(object):
         # self.server = server if server else Server()
         pass
 
-    def display(self, graph):
-        self.fig = plt.figure()
-        graph.display(self)
+    def display(self, *graphes):
+        self.subconf, self.subfigsize = self.createSubConf(graphes)
+        self.fig = plt.figure(figsize=(8, 8))
+        for i, graph in enumerate(graphes):
+            vax = self.createAxis(graph, i)
+            graph.display(vax)
+    
+    def createAxis(self, graph, i):
+        kwargs = {}
+        if graph.dim() >= 3:
+            kwargs['projection'] = '3d'
+        ax = self.fig.add_subplot(*self.subconf, i + 1, **kwargs)
+        vax = VisualAxis(self, ax)
+
+        # if graph.ratio == 'square':
+        ax.set_aspect('equal')
+
+        ax.yaxis.set_major_formatter(
+            mpl.ticker.StrMethodFormatter('{x:,.2f}'))
+        ax.grid(True)
+
+        return vax
+    
+    def createSubConf(self, graphes):
+        number = len(graphes)
+        # horizontals = len(list(graph for graph in graphes if graph.ratio == 'horizontal'))
+        # verticals = len(list(graph for graph in graphes if graph.ratio == 'vertical'))
+        m = 1
+        while number > m ** 2:
+            m += 1
+        n = math.ceil(number / m)
+
+        return ((n, m), (15, 15))
+
+
+class VisualAxis(object):
+    def __init__(self, visual, ax):
+        self.visual = visual
+        self.ax = ax
 
 
 class Graph(object):
-    def __init__(self, *items):
+    def __init__(self, *items, title=None, ratio=None, options=None):
         super().__init__()
         self.items = list(items)
-    
+        options = parameter(options, {})
+
+        self.range = options.get('range')
+        self.title = title
+        self.ratio = ratio  # can be 'square', 'horizontal', 'vertical'
+
     def __add__(self, other):
         return self.__class__(*(self.items + other.items))
-    
+
     def display(self, visual):
-        visual.ax = visual.fig.add_subplot(1, 1, 1)
-        visual.ax.yaxis.set_major_formatter(
-            mpl.ticker.StrMethodFormatter('{x:,.3f}'))
+        plt.title(self.title)
+
         for item in self.items:
             item.display(visual)
-    
+
     def plot(self, *args, **kwargs):
         self.items.append(PlotItem(*args, **kwargs))
+        return self
+    
+    def scatter(self, *args, **kwargs):
+        self.items.append(ScatterItem(*args, **kwargs))
+        return self
+    
+    def rectangle(self, *args, **kwargs):
+        kwargs['shape'] = 'rectangle'
+        self.items.append(PatchItem(*args, **kwargs))
+        return self
+    
+    def dim(self):
+        return max((item.dim for item in self.items))
 
 
 class GraphItem(object):
@@ -51,9 +110,48 @@ class PlotItem(GraphItem):
         self.dim = len(self.data)
         self.options = options
     
-    def display(self, visual):
+    def display(self, plotter):
         plt.xlabel('Iteration')
         if self.dim == 1:
-            visual.ax.plot(self.data[0])
+            plotter.ax.plot(self.data[0])
         elif self.dim == 2:
-            visual.ax.plot(self.data[0], self.data[1])
+            plotter.ax.plot(self.data[0], self.data[1])
+
+
+class ScatterItem(GraphItem):
+    def __init__(self, *data, **options):
+        super().__init__()
+        if len(data) == 1 and isinstance(data[0], np.ndarray):
+            self.data = data[0]
+        else:
+            self.data = np.array(data)
+        self.dim = self.data.shape[1]
+        self.options = options
+
+    def display(self, plotter):
+        if self.dim == 1:
+            plotter.ax.scatter(self.data[:, 0])
+        elif self.dim == 2:
+            plotter.ax.scatter(self.data[:, 0], self.data[:, 1])
+        elif self.dim >= 2:
+            plotter.ax.scatter(self.data[:, 0], self.data[:, 1], self.data[:, 2])
+
+
+class PatchItem(GraphItem):
+    def __init__(self, position, width, height, shape='rectangle', **options):
+        super().__init__()
+        self.position = position
+        self.width = width
+        self.height = height
+        self.shape = shape
+
+        self.dim = 2
+        self.options = options
+
+    def display(self, plotter):
+        if self.shape == 'rectangle':
+            plotter.ax.add_patch(patches.Rectangle(self.position,
+                                                   self.width,
+                                                   self.height,
+                                                   alpha=self.options.get('alpha'),
+                                                   zorder=self.options.get('zorder')))
